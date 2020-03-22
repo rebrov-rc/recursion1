f18a5ef15cb51249bdc5cdc2c93c22ff7ea9d81a
Roman <61121402+rebrov-rc@users.noreply.github.com>
2020-03-22 22:55:59 +0300

    22.03 22.55

---
+++ recursion.js
@@ -0,0 +1,17 @@
+let x = 4501, y, n;
+
+function isEven(x) {
+  if ( x == 0 ) {
+      return true;
+  }else if( x == 1 ) {
+      return false;
+      
+  } else if ( x < 0 ) {
+return isEven(x + 2);
+  }else {
+    return isEven(x - 2);
+    
+  }
+   
+}
+console.log(isEven(-52));
